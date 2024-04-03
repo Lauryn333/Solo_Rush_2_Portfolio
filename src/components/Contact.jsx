1b4c71ecf14a2e0a32b82895e005fc14f8e94490
Lauryn MARTIN <lauryn.martin333@gmail.com>
2024-04-03 10:12:47 +0200

    Add Contact.jsx and CSS in App.css

---
+++ src/components/Contact.jsx
@@ -0,0 +1,22 @@
+
+
+function Contact() {
+
+    return (
+        <form>
+            <label>Nom-Prénom </label>
+            <input />
+
+            <label>Mail </label>
+            <input />
+
+            <label>Message </label>
+            <textarea ></textarea>
+
+            <button className="button_form">Envoyer</button>
+        </form>
+    )
+
+}
+
+export default Contact;
---
+++ src/components/Languages.jsx
@@ -0,0 +1,7 @@
+function Languages() {
+    return (
+        <p>Coucou</p>
+    )
+
+}
+export default Languages;
